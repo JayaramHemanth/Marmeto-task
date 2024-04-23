214e3793c7b2381800ced12d9f726a6f034bf296
Hemanth J <hemanthsree555@gmail.com>
2024-04-24 00:58:05 +0530

    fixed

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   // Show default category content (Men)
   fetchData("Men");
 
-  // Activate Men tab
-  document.getElementById("Men").classList.add("active");
+  // Activate Men tab and show its content
+  document.getElementById("MenTab").classList.add("active");
+  openCategory(event, 'Men');
 });
 
 function openCategory(evt, categoryName) {
@@ -39,9 +41,7 @@ function openCategory(evt, categoryName) {
 
 function fetchData(categoryName) {
   // Simulating fetching data from API
-  fetch(
-    "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
-  )
+  fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json")
     .then((response) => response.json())
     .then((data) => displayCategoryData(data, categoryName))
     .catch((error) => console.error("Error fetching data:", error));
@@ -56,28 +56,27 @@ function displayCategoryData(data, categoryName) {
 
   categoryData.category_products.forEach((product) => {
     const percentageDiscount =
-      ((product.compare_at_price - product.price) / product.compare_at_price) *
-      100;
+      ((product.compare_at_price - product.price) / product.compare_at_price) * 100;
     const itemElement = document.createElement("div");
     itemElement.classList.add("item");
     itemElement.innerHTML = `
-        <div style="position: relative;">
-          <img src="${product.image}" alt="${product.title}">
-          <p class="badge">${product.badge_text === null ? "" : product.badge_text}</p>
-        </div>
-        <div class="title">
-          <h4>${product.title}</h4>
-          <h4 class="vendor">${product.vendor}</h4>
-        </div>
-        <div class="price">
-          <p>Rs ${product.price}.00</p>
-          <p class="extra_price">${product.compare_at_price}.00</p>
-          <p class="disc">${percentageDiscount.toFixed(2)}% Off</p>
-        </div>
-        <div>
-          <button class="btn">Add to Cart</button>
-        </div>
-      `;
+      <div style="position: relative;">
+        <img src="${product.image}" alt="${product.title}">
+        <p class="badge">${product.badge_text === null ? "" : product.badge_text}</p>
+      </div>
+      <div class="title">
+        <h4>${product.title}</h4>
+        <h4 class="vendor">${product.vendor}</h4>
+      </div>
+      <div class="price">
+        <p>Rs ${product.price}.00</p>
+        <p class="extra_price">${product.compare_at_price}.00</p>
+        <p class="disc">${percentageDiscount.toFixed(2)}% Off</p>
+      </div>
+      <div>
+        <button class="btn">Add to Cart</button>
+      </div>
+    `;
     categoryContent.appendChild(itemElement);
   });
 }
